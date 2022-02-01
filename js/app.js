6db1581ad8fb535b60d1ae843e70abf5d7972d7b
Ferhat KAYMAZ <ferhatkaymaz2023@gmail.com>
2022-02-01 20:23:22 +0300

    second  commit no description

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('film-form');
+const titleElement = document.getElementById('title');
+const directorElement = document.getElementById('director');
+const urlElement = document.getElementById('url');
+const moviesList = document.getElementById('films');
+const clearButton = document.getElementById('clear-films');
+
+(() => {
+
+})();
+
+
